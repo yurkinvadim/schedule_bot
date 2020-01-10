67203977a8d68e33ded2b82c5a780174df130cc3
Vadim <yurkinvadim99@gmail.com>
2020-01-10 04:12:16 +0300

    current_week()
    Добавил определение текущей недели, не забудь установить bs4 и html5lib  (хотя не уверен что второе используется, но на всякий)

--- schedules/miu_schedule.py
+++ schedules/miu_schedule.py
@@ -1,12 +1,25 @@
 import requests
 import pandas
+from bs4 import BeautifulSoup
+import requests
+import html5lib
+
+
+def current_week():
+    html = requests.get('http://miu.by/rus/schedule/schedule.php').text
+    soup = BeautifulSoup(html, 'html5lib')
+    # во всех div найти span, где первый элемент "ТЕКУЩАЯ НЕДЕЛЯ: N",
+    # откидываем первые 17 символов
+    return [span for div in soup('div') for span in div('span')][1].text[17:]
 
 
 def schedule_parameters():
     expschedule_url = "http://miu.by/rus/schedule/expshedule.php"
     payload = {'spec': 'Программное обеспечение информационных технологий'.encode('windows-1251'),
                'group': '161701с'.encode('windows-1251'),
-               'week': '19'}
+               'week': current_week()}
+# 'week': '19'}
+    
     headers = {"Content-type": "application/x-www-form-urlencoded"}
     csv = requests.request("POST", url=expschedule_url, headers=headers, data=payload)
     csv.encoding = 'windows-1251'
